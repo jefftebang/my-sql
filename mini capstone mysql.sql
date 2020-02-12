4199f460fe3d6c84e08e8ef816bec384d738e94a
fish-eye <jefftebang0118@gmail.com>
2020-02-12 15:18:06 +0800

    edited minicapstonesql.sql

--- mini capstone mysql.sql
+++ mini capstone mysql.sql
@@ -1,11 +1,16 @@
-it and hr
+1. SELECT firstName, lastName, branches.name AS branchName from Employees 
+JOIN branch_employee ON (employees.id = branch_employee.employee_id)
+JOIN branches ON (branches.id = branch_employee.branch_id)
+WHERE rank_id = (SELECT id FROM ranks WHERE name = "junior") AND id IN (SELECT id FROM branches 
+JOIN branch_employee ON (branches.id = branch_employee.branch_id)
+WHERE name IN ("QC", "Ortigas")) ;
 
-update employees set salary += 1000 where role_id = 3
-(select firstName, lastName from employees where employee_id = 
-(select employee_id from department_employee where department_id =
-(select id from departments where name in ("hr", "it"))));
-
-
--- bonus
--- 6)
-delete from employees where role_id = 3
+2. UPDATE employees SET salary += 1000
+	WHERE id IN (
+	SELECT id FROM employees
+	JOIN roles ON (employees.role_id = roles.id)  
+	JOIN department_employees ON (employees.id = department_employee.employee_id)
+	JOIN departments ON (department_employee.department_id = departments.id)
+	WHERE role_id = 
+ (SELECT id FROM roles WHERE name = "superadmin") 
+ AND department_employee.department_id IN (SELECT id FROM departments WHERE name = ALL ("IT","HR" )));
